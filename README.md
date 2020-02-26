0f62926ffd2d4fbe9c38441ae126ac66391d4337
Phonekham <phone.madridista@gmail.com>
2020-02-26 16:10:31 +0700

    Schemas & Resolvers

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mernQL-eventBooking
- Building App with MERN Stack with GraphQL
+
+Building App with MERN Stack with GraphQL
+
+## NPM Packages
+
+### Backend
+
+body-parser<br>
+express<br>
+express-graphql<br>
+graphql
--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
-const app = express();
-app.use(bodyParser.json());
+var schema = buildSchema(`
+  type Query {
+    events: [String!]!
+  }
 
-app.listen(3000);
+   type Mutation {
+    createEvent(name: String): String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  events: () => {
+    return ["React", "Node", "Graphql"];
+  },
+  createEvent: args => {
+    const eventName = args.name;
+    return eventName;
+  }
+};
+
+var app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
