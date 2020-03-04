b70ce07cb1764aa4878fe1d85df236a2f7981b07
Phonekham <phone.madridista@gmail.com>
2020-03-04 14:30:18 +0700

    Navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,26 @@ import "./App.css";
 import AuthPage from "./pages/Auth";
 import EventsPage from "./pages/Events";
 import BookingsPage from "./pages/Bookings";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact></Redirect>
-        <Route path="/auth" component={AuthPage}></Route>
-        <Route path="/events" component={EventsPage}></Route>
-        <Route path="/bookings" component={BookingsPage}></Route>
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNavigation></MainNavigation>
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact></Redirect>
+              <Route path="/auth" component={AuthPage}></Route>
+              <Route path="/events" component={EventsPage}></Route>
+              <Route path="/bookings" component={BookingsPage}></Route>
+            </Switch>
+          </main>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
